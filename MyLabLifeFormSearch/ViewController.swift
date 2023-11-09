19b3076cb642f482916ce05a92b4ef42a8483698
wayne tsao <a578ff6@gmail.com>
2023-11-09 23:58:22 +0800

    Initial Commit

---
+++ MyLabLifeFormSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLabLifeFormSearch
+//
+//  Created by 曹家瑋 on 2023/11/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
